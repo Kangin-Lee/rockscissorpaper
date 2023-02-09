a017c982450a9279f9b432059115c785d050ab85
Kangin Lee <cjsworkddls1@naver.com>
2023-02-09 19:26:27 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -18,21 +18,51 @@ const choice={
 }
 function App() {
   const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const paly = (userChoice) => {
+    setUserSelect(choice[userChoice]);
 
-  const paly=(userChoice)=>{
-    setUserSelect(choice[userChoice])
-  }
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const judgement = (user, computer) => {
+    if (user.name == computer.name) {
+      return "tie";
+    } else if (user.name == "Rock")
+      return computer.name == "Scissors" ? "Win" : "Lose";
+    else if (user.name == "Scissor")
+      return computer.name == "Paper" ? "Win" : "Lose";
+    else if (user.name == "Paper")
+      return computer.name == "Scissors" ? "Win" : "Lose";
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); // 객체에 키값만 뽑아 배열로 만들어 주는 함수
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
   return (
     <>
-    <div className="main">
-      <Box title="You" item={userSelect} />
-      {/* <Box title="Computer"/> */}
-    </div>
-    <div className="main">
-      <button onClick={()=>paly("scissors")} className="btn"><img src="https://nehalhazem.github.io/rockPaperScissors.io/img/scissors.png" /></button>
-      <button onClick={()=>paly("rock")} className="btn"><img src="https://nehalhazem.github.io/rockPaperScissors.io/img/rock.png"/></button>
-      <button onClick={()=>paly("paper")} className="btn"><img src="https://nehalhazem.github.io/rockPaperScissors.io/img/paper.png"/></button>
-    </div>
+      <div className="main">
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
+      </div>
+      <div className="main">
+        <button onClick={() => paly("scissors")} className="btn">
+          <img src="https://nehalhazem.github.io/rockPaperScissors.io/img/scissors.png" />
+        </button>
+        <button onClick={() => paly("rock")} className="btn">
+          <img src="https://nehalhazem.github.io/rockPaperScissors.io/img/rock.png" />
+        </button>
+        <button onClick={() => paly("paper")} className="btn">
+          <img src="https://nehalhazem.github.io/rockPaperScissors.io/img/paper.png" />
+        </button>
+      </div>
     </>
   );
 }
